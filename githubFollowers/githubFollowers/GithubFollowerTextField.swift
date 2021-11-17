141e85b81e2d0fa5177df6077250a45fd29b024a
babywalnut <anfuza@naver.com>
2021-11-17 20:41:26 +0900

    Feat: GFButton & GFTextField 추가 GitHubFollowerButton.swift: 	- configure 메서드 추가, custom initializer 추가 GitHubFollowersTextField.swift: 	- configure 메서드 추

---
+++ githubFollowers/githubFollowers/GithubFollowerTextField.swift
@@ -0,0 +1,40 @@
+//
+//  GithubFollowerTextField.swift
+//  githubFollowers
+//
+//  Created by 김민성 on 2021/11/17.
+//
+
+import UIKit
+
+class GithubFollowerTextField: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter a username"
+    }
+}
