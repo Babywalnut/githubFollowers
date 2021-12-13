0deec421d0bec469233d1ac5778e171f11adffc3
babywalnut <anfuza@naver.com>
2021-12-13 22:36:20 +0900

    Feat: Model 구현 Follower.swift: 	- Follower 모델 User.swift: 	- User 모델

---
+++ githubFollowers/githubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  githubFollowers
+//
+//  Created by 김민성 on 2021/12/13.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ githubFollowers/githubFollowers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  githubFollowers
+//
+//  Created by 김민성 on 2021/12/13.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
