a086edc2fcd8af5de23f9672a5ce800d5ce2fa06
babywalnut <anfuza@naver.com>
2021-12-27 00:59:07 +0900

    Refactor: NetworkManager 에러타입 -> Result 타입 수정 NetworkManager.swift: 	- completion Handler로 전달되는 인자의 타입을 Result 타입으로 수정 FollowerListViewController.swift: 	- NetworkManager의 getFollowers메서드 호출시 switch - case로 처리

--- githubFollowers/githubFollowers/Screens/FollwerListViewController.swift
+++ githubFollowers/githubFollowers/Screens/FollwerListViewController.swift
@@ -16,13 +16,15 @@ class FollwerListViewController: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
-            guard let followers = followers else {
-                self.presentGithubFollwerAlertOnMainThread(title: "Bad Stuff Happened", message: errorMessage!.rawValue, buttonTitle: "Ok")
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            
+            switch result {
+            case .success(let followers):
+                print("Followers.count = \(followers.count)")
+                print(followers)
+            case .failure(let error):
+                self.presentGithubFollwerAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
             }
-            print("Followers.count = \(followers.count)")
-            print(followers)
         }
     }
 
