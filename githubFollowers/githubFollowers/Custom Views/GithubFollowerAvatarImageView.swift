69a73d53a98a992f1b41f092edff5049fb4a1c07
babywalnut <anfuza@naver.com>
2021-12-31 02:16:26 +0900

    Feat: FollwerCell & AvatarImageView 구현 FollwerCell.swift: 	- set(): API에서 받아온 데이터를 적용할때 사용

---
+++ githubFollowers/githubFollowers/Custom Views/GithubFollowerAvatarImageView.swift
@@ -0,0 +1,29 @@
+//
+//  GithubFollowerAvatarImageView.swift
+//  githubFollowers
+//
+//  Created by 김민성 on 2021/12/31.
+//
+
+import UIKit
+
+class GithubFollowerAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
