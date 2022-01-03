9e1e4fa5ba9f12641efb9ea6490ab86fecbb9812
babywalnut <anfuza@naver.com>
2022-01-03 14:26:03 +0900

    Feat: EmptyStateView 구현 GithubFollowerEmptyStateView.swift: 	- 네트워크 통신에 의한 follower 결과가 없을시 출력할 view FollowerListViewController.swift: 	- 네트워크 통신을 통해 받아온 데이터를 VC에 갱신후 EmptyView처리

---
+++ githubFollowers/githubFollowers/Custom Views/Views/GithubFollowerEmptyStateView.swift
@@ -0,0 +1,52 @@
+//
+//  GithubFollowerEmptyStateView.swift
+//  githubFollowers
+//
+//  Created by 김민성 on 2022/01/03.
+//
+
+import UIKit
+
+class GithubFollowerEmptyStateView: UIView {
+    
+    let messageLabel = GithubFollowerTitleLabel(textAlignment: .center, fontSize: 28)
+    let logoImageView = UIImageView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(message: String) {
+        super.init(frame: .zero)
+        messageLabel.text = message
+        configure()
+    }
+    
+    private func configure() {
+        addSubview(messageLabel)
+        addSubview(logoImageView)
+        
+        messageLabel.numberOfLines = 0
+        messageLabel.textColor = .secondaryLabel
+        
+        logoImageView.image = UIImage(named: "empty-state-logo")
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -150),
+            messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
+            messageLabel.heightAnchor.constraint(equalToConstant: 200),
+            
+            logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
+            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 40)
+        ])
+    }
+}
